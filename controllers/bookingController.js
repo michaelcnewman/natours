948f569432edd81b3db11a72c53b1ef6e16b869d
Newman <newman039@gmail.com>
2020-06-19 13:31:01 -0400

    Integrated Stripe Webhooks3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -20,7 +20,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
             {
                 name: `${tour.name} Tour`,
                 description: `${tour.summary}`,
-                images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+                images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
                 amount: tour.price * 100,
                 currency: 'usd',
                 quantity: 1,
@@ -48,7 +48,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
     await Booking.create({ tour, user, price });
 };
 
@@ -63,13 +63,13 @@ exports.webhookCheckout = async (req, res, next) => {
             signature,
             process.env.STRIPE_WEBHOOK_SECRET
         );
+
+        if (event.type === 'checkout.session.completed') {
+            await createBookingCheckout(event.data.object);
+            res.status(200).json({ received: true });
+        }
     } catch (err) {
         return res.status(400).send(`Webhook error: ${err.message}`);
-    }
-
-    if (event.type === 'checkout.session.complete') {
-        await createBookingCheckout(event.data.object);
-        res.status(200).json({ received: true });
     }
 };
 
